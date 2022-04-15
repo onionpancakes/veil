ff11073e9768e6f44eb167ce7a2d7ea91948eed0
onionpancakes <639985+onionpancakes@users.noreply.github.com>
2022-04-14 23:10:50 -0700

    switch back to v ns alias; rm com ns

--- test/dev/onionpancakes/veil/test_core.clj
+++ test/dev/onionpancakes/veil/test_core.clj
@@ -1,9 +1,9 @@
 (ns dev.onionpancakes.veil.test-core
-  (:require [dev.onionpancakes.veil.core :as c]
+  (:require [dev.onionpancakes.veil.core :as v]
             [clojure.test :refer [deftest are]]))
 
 (deftest test-compile
-  (are [x y] (= (c/compile* x) y)
+  (are [x y] (= (v/compile* x) y)
     [:div]            '(js/React.createElement "div" nil)
     [:div nil]        '(js/React.createElement "div" nil)
     [:div nil "foo"]  '(js/React.createElement "div" nil "foo")
@@ -27,14 +27,14 @@
                            (js/React.createElement "li" nil i)))))
 
 (deftest test-compile-user-defined-components
-  (are [x y] (= (c/compile* x) y)
+  (are [x y] (= (v/compile* x) y)
     [:Foo]            '(js/React.createElement Foo nil)
     [:foo.bar]        '(js/React.createElement foo.bar nil)
     [::Foo]           `(js/React.createElement Foo nil)
     [:foo/Bar]        '(js/React.createElement foo/Bar nil)))
 
 (deftest test-compile-no-props
-  (are [x y] (= (c/compile* x) y)
+  (are [x y] (= (v/compile* x) y)
     [:div "foo"]  '(js/React.createElement "div" nil "foo")
     [:div 'foo]   '(js/React.createElement "div" nil foo)
     [:div [:div]] '(js/React.createElement "div" nil
@@ -43,7 +43,7 @@
 
 (deftest test-compile-js-props
   ;; Compare props map
-  (are [x y] (= (some-> (c/compile* x) (nth 2) (.val)) y)
+  (are [x y] (= (some-> (v/compile* x) (nth 2) (.val)) y)
     [:div "foo"]             nil
     [:div nil "foo"]         nil
     [:div {} "foo"]          {}
